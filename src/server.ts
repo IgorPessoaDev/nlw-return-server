d55d3640d454354cd1795e3c3c178b354f4f0636
IgorPessoa <igorpessoa.dev@gmail.com>
2022-05-12 21:31:45 -0300

    edit

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,6 @@ app.use(cors({
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => {
-  console.log("Server Rodando na porta 3333");
+app.listen(process.env.PORT || 3333, () => {
+  console.log("HTTP server running!");
 })
